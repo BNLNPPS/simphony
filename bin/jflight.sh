5eeb84b66c85da0accf2109b073400de38a105c5
Dmitri Smirnov <dmixsmi@gmail.com>
2026-03-25 08:00:55 -0400

    refactor(bin): remove unused shell scripts (#239)

--- bin/jflight.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -l 
-
-
-OGeo=INFO OpticksDbg=INFO  FLIGHT=RoundaboutXY PERIOD=8 EMM=~8, PVN=lLowerChimney_phys flight.sh --rtx 1 --cvd 1 
-
--- bin/ls.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while true; do 
-   test -d $USER && date && sleep 10000000000
-done
-
