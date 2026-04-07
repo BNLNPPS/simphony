fa816eef5c4ffb4592582a5afe268b17e08f1e2d
Gabor Galgoczi <gaborgalgoczi@gmail.com>
2026-04-07 14:55:29 +0000

    revert primary energy to 5 GeV for CI raindrop test
    The 10 MeV change was for APEX validation and belongs on the
    physics branch, not analysis-tools.

--- src/GPURaytrace.h
+++ src/GPURaytrace.h
@@ -306,7 +306,7 @@ struct PrimaryGenerator : G4VUserPrimaryGeneratorAction
 
         G4PrimaryVertex *vertex = new G4PrimaryVertex(position_mm, time_ns);
         G4PrimaryParticle *particle = new G4PrimaryParticle(G4Electron::Definition());
-        particle->SetKineticEnergy(10 * MeV);
+        particle->SetKineticEnergy(5 * GeV);
         particle->SetMomentumDirection(direction);
         vertex->SetPrimary(particle);
         event->AddPrimaryVertex(vertex);
