4d30762b787e998c1eb16cfccd1bd842ba1f26ef
Dmitri Smirnov <dmixsmi@gmail.com>
2026-06-18 16:36:58 -0400

    build: replace OpenSSL MD5 dependency with vendored Solar MD5
    - move the Solar Designer MD5 implementation from `sysrap` into `third_party/solarmd5`
    - replace OpenSSL MD5 includes with the vendored Solar MD5 header
    - remove OpenSSL from SysRap/QUDARap link dependencies and exported package dependencies
    - document the upstream Openwall source and GitHub mirror for the vendored MD5 files

---
+++ third_party/solarmd5/README.md
@@ -0,0 +1,14 @@
+# Solar Designer MD5
+
+This directory contains the OpenSSL-compatible public-domain MD5 implementation
+by Alexander Peslyak, also known as Solar Designer.
+
+Upstream homepage:
+http://openwall.info/wiki/people/solar/software/public-domain-source-code/md5
+
+GitHub mirror:
+https://github.com/openwall/popa3d/tree/main/md5
+
+The vendored `md5.c` and `md5.h` files match the GitHub mirror byte for byte.
+
+The implementation is vendored here because it is external third-party code.
--- third_party/solarmd5/md5.h
+++ third_party/solarmd5/md5.h
@@ -1,34 +1,4 @@
 /*
- * Copyright (c) 2019 Opticks Team. All Rights Reserved.
- *
- * This file is part of Opticks
- * (see https://bitbucket.org/simoncblyth/opticks).
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License.  
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
- * See the License for the specific language governing permissions and 
- * limitations under the License.
- */
-
-//  see solarmd5-
-
-/**
-md5
-=====
-
-Updatable digest hash mechanics.
-
-
-**/
-
-/*
  * This is an OpenSSL-compatible implementation of the RSA Data Security, Inc.
  * MD5 Message-Digest Algorithm (RFC 1321).
  *
@@ -57,7 +27,6 @@ Updatable digest hash mechanics.
 #include <openssl/md5.h>
 #elif !defined(_MD5_H)
 #define _MD5_H
-
 
 /* Any 32-bit or wider unsigned integer data type will do */
 typedef unsigned int MD5_u32plus;
