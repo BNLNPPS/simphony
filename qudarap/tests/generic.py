2938c0693e41250e00cd1f2433db601ff9b2dbfb
Dmitri Smirnov <dmixsmi@gmail.com>
2026-06-22 10:51:21 -0400

    remove underutilized python scripts

--- qudarap/tests/generic.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import os, numpy as np
-from opticks.ana.fold import Fold
-
-if __name__ == '__main__':
-    t = Fold.Load(symbol="t", globals=True)
-    print(repr(t))
-pass 
--- sysrap/tests/SEvt_AddEnvMeta_Test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import numpy as np
-from opticks.ana.fold import Fold
-
-if __name__ == '__main__':
-    t = Fold.Load(symbol="t")
-    print(repr(t))
-pass 
